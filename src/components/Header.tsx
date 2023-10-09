064cb3438fb72be0d1057aa37b1c9b44a5c4f7d5
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-10-09 12:06:45 -0300

    ♻️ Adjustment in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,27 +2,39 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="container mx-auto">
-      <nav className="flex justify-between h-16 items-center bg-">
-        <Image src="" alt="Logo Carlos Silva Design Rústico" />
-        <ul className="flex">
-          <li>
-            <a className="px-2" href="#">
-              Serviços
-            </a>
-          </li>
-          <li>
-            <a className="px-2" href="#">
-              Sobre
-            </a>
-          </li>
-          <li>
-            <a className="px-2" href="#">
-              Contato
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <header className="bg-[#d18448]">
+      <div className="container mx-auto">
+        <nav className="container flex justify-between h-16 items-center ">
+          {/* <Image src="" alt="Logo Carlos Silva Design Rústico" /> */}
+          <p className="text-white text-2xl">CS Design Rústico</p>
+          <ul className="flex">
+            <li>
+              <a
+                className="px-2 font-semibold text-white uppercase transition-all duration-300 ease-linear hover:text-black"
+                href="#"
+              >
+                Serviços
+              </a>
+            </li>
+            <li>
+              <a
+                className="px-2 font-semibold text-white uppercase transition-all duration-300 ease-linear hover:text-black"
+                href="#"
+              >
+                Sobre
+              </a>
+            </li>
+            <li>
+              <a
+                className="px-2 font-semibold text-white uppercase transition-all duration-300 ease-linear hover:text-black"
+                href="#"
+              >
+                Contato
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
