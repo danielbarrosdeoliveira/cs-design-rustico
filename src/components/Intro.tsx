9c4f10472187b58ceb81e0c1298328cb4e945ee7
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-11-16 16:37:12 -0300

    change intro bg

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@ export const Intro = () => {
   return (
     <section
       id="intro"
-      className="bg-[url('https://images.unsplash.com/photo-1612142792991-18f8e0fe4526?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-cover bg-center h-[480px] w-screen flex justify-center items-center"
+      className="bg-[url('/images/bg-intro.jpg')] bg-no-repeat bg-cover bg-center h-[480px] w-screen flex justify-center items-center"
     >
       <h2 className="text-5xl text-white tracking-wider">Arte em Pedras</h2>
     </section>
