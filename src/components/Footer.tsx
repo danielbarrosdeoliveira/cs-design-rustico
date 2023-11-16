f350ae35aca583f68033499174461b5eddc0f73e
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-11-16 16:35:33 -0300

    footer created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center h-24 bg-[#d18448] mt-12">
+      <p className="text-white">
+        copyright &copy; CS Designer Rústico - Todos os Direitos Reservados
+      </p>
+      <p className="text-white">
+        Desenvolvido por{" "}
+        <a
+          href="https://vianait.com.br"
+          target="_blank"
+          className="transition-all duration-150 ease-linear hover:text-black"
+        >
+          Viana IT
+        </a>
+      </p>
+    </footer>
+  );
+};
