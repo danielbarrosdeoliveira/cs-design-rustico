8c1202f791cf8d803e069110a2b9c049a4d5ea5e
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-11-20 16:19:51 -0300

    adjustment services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,107 +1,111 @@
 export const Services = () => {
   return (
-    <section id="services" className="container mx-auto mt-16">
-      <h1 className="text-3xl text-center font-bold uppercase">Serviços</h1>
-      <div className="flex gap-4 mt-8 flex-col lg:flex-row px-6 sm:px-0">
-        <div className="flex flex-col gap-4">
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Descubra a Elegância Lapidada em Pedra
-            </h3>
-            <p className="text-lg text-center">
-              Seu espaço merece mais do que simples construções; merece obras de
-              arte que resistam ao teste do tempo...
-            </p>
+    <section id="services" className="mt-16">
+      <div className="container mx-auto">
+        <h1 className="text-3xl text-center font-bold uppercase">Serviços</h1>
+
+        <h2 className="text-1xl text-center uppercase font-bold mt-4">
+          Descubra a Elegância Lapidada em Pedra
+        </h2>
+        <p className="text-lg text-center">
+          Seu espaço merece mais do que simples construções; merece obras de
+          arte que resistam ao teste do tempo...
+        </p>
+        <div className="flex gap-4 mt-10 flex-col lg:flex-row px-6 sm:px-0">
+          <div className="flex flex-col gap-4">
+            <div></div>
+
+            <div>
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Revestimento em Pedra:
+              </h3>
+              <p className="text-lg text-center">
+                Seja em fachadas imponentes ou na parede interna que anseia por
+                um toque refinado, nosso revestimento em pedra oferece uma
+                estética única que transcende as tendências passageiras.
+              </p>
+            </div>
+
+            <div>
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Muros que Contam Histórias:
+              </h3>
+              <p className="text-lg text-center">
+                Nossos muros não são meros limites físicos; são narrativas
+                esculpidas em pedra, contando a história da sua propriedade com
+                elegância e solidez.
+              </p>
+            </div>
+
+            <div>
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Cascatas que Encantam:
+              </h3>
+              <p className="text-lg text-center">
+                O suave som da água cascata em pedras meticulosamente dispostas,
+                criando uma atmosfera de serenidade e refinamento. Nossas
+                cascatas transformam seu espaço em um oásis personalizado.
+              </p>
+            </div>
           </div>
 
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Revestimento em Pedra:
-            </h3>
-            <p className="text-lg text-center">
-              Seja em fachadas imponentes ou na parede interna que anseia por um
-              toque refinado, nosso revestimento em pedra oferece uma estética
-              única que transcende as tendências passageiras.
-            </p>
-          </div>
+          <div className="flex flex-col gap-4">
+            <div className="">
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Calçamentos com Personalidade:
+              </h3>
+              <p className="text-lg text-center">
+                Pisos não são apenas para caminhar; são para impressionar.
+                Oferecemos calçamentos que não só suportam o peso do dia a dia,
+                mas também elevam a estética do seu ambiente exterior.
+              </p>
+            </div>
 
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Muros que Contam Histórias:
-            </h3>
-            <p className="text-lg text-center">
-              Nossos muros não são meros limites físicos; são narrativas
-              esculpidas em pedra, contando a história da sua propriedade com
-              elegância e solidez.
-            </p>
-          </div>
+            <div>
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Escadas que Conduzem à Grandeza:
+              </h3>
+              <p className="text-lg text-center">
+                Cada degrau é uma declaração de elegância e funcionalidade.
+                Nossas escadas em pedra são mais do que meios de acessibilidade;
+                são uma expressão de estilo.
+              </p>
+            </div>
 
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Cascatas que Encantam:
-            </h3>
-            <p className="text-lg text-center">
-              O suave som da água cascata em pedras meticulosamente dispostas,
-              criando uma atmosfera de serenidade e refinamento. Nossas cascatas
-              transformam seu espaço em um oásis personalizado.
-            </p>
-          </div>
-        </div>
+            <div>
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Grutas que Despertam a Imaginação:
+              </h3>
+              <p className="text-lg text-center">
+                Em seu jardim ou área de lazer, criamos grutas que transportam
+                você para um mundo de maravilhas. Uma fusão perfeita entre
+                natureza e artesanato em pedra.
+              </p>
+            </div>
 
-        <div className="flex flex-col gap-4">
-          <div className="">
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Calçamentos com Personalidade:
-            </h3>
-            <p className="text-lg text-center">
-              Pisos não são apenas para caminhar; são para impressionar.
-              Oferecemos calçamentos que não só suportam o peso do dia a dia,
-              mas também elevam a estética do seu ambiente exterior.
-            </p>
-          </div>
-
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Escadas que Conduzem à Grandeza:
-            </h3>
-            <p className="text-lg text-center">
-              Cada degrau é uma declaração de elegância e funcionalidade. Nossas
-              escadas em pedra são mais do que meios de acessibilidade; são uma
-              expressão de estilo.
-            </p>
-          </div>
-
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Grutas que Despertam a Imaginação:
-            </h3>
-            <p className="text-lg text-center">
-              Em seu jardim ou área de lazer, criamos grutas que transportam
-              você para um mundo de maravilhas. Uma fusão perfeita entre
-              natureza e artesanato em pedra.
-            </p>
-          </div>
-
-          <div>
-            <h3 className="text-1xl text-center uppercase font-bold">
-              Lagos que Refletem Serenidade:
-            </h3>
-            <p className="text-lg text-center">
-              Nossos lagos em pedra não são apenas corpos d&apos;água; são
-              espelhos serenos que refletem a tranquilidade e a beleza que sua
-              propriedade merece.
-            </p>
+            <div>
+              <h3 className="text-1xl text-center uppercase font-bold">
+                Lagos que Refletem Serenidade:
+              </h3>
+              <p className="text-lg text-center">
+                Nossos lagos em pedra não são apenas corpos d&apos;água; são
+                espelhos serenos que refletem a tranquilidade e a beleza que sua
+                propriedade merece.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-
-      <p className="mt-8 text-lg text-center px-6 sm:px-0">
-        Deixe-nos esculpir a essência da elegância em cada pedra, transformando
-        seus espaços em testemunhas duradouras de arte e qualidade. Estamos aqui
-        para transcender expectativas e criar experiências visuais que resistem
-        à passagem do tempo. Descubra o poder da arte em pedra, transforme seu
-        ambiente, transforme sua vida.
-      </p>
+      <div className="bg-[#d18448] text-white">
+        <p className="container mx-auto mt-10 py-4 text-lg text-center italic px-6 sm:px-0">
+          &quot;Deixe-nos esculpir a essência da elegância em cada pedra,
+          transformando seus espaços em testemunhas duradouras de arte e
+          qualidade. Estamos aqui para transcender expectativas e criar
+          experiências visuais que resistem à passagem do tempo. Descubra o
+          poder da arte em pedra, transforme seu ambiente, transforme sua
+          vida.&quot;
+        </p>
+      </div>
     </section>
   );
 };
