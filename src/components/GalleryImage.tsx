342ac5599c9c1bbb409256eae6866dc19c880b09
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-11-20 15:48:21 -0300

    added padding to small dvies

--- src/components/GalleryImage.tsx
+++ src/components/GalleryImage.tsx
@@ -5,7 +5,7 @@ import { photos, photos2 } from "@/utils/photos";
 
 export const GalleryImage = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-2 sm:px-0 mt-16">
       <Gallery photos={photos} direction={"row"} margin={8} columns={4} />
       <Gallery photos={photos2} direction={"row"} margin={8} />
     </div>
